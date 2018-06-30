451243ad06b0311bacd35286634e13407f7352e8
smackjax <smackjax@gmail.com>
2018-06-30 16:23:16 -0400

    Fix data response shape

--- ezee-ajax-emailer/send-email.php
+++ ezee-ajax-emailer/send-email.php
@@ -108,13 +108,11 @@ function respond($code, $data_obj, $status){
     $data_obj['status'] = $status;
     echo json_encode($data_obj);
 }
-function respond_server_error($message){
-    $data = [];
-    $data['message'] = $message;
-    respond(500, $data, 'error');
-}
 function respond_user_error($data_obj){
-    respond(400, $data_obj, 'fail');
+    $response = [
+        'data'=>$data_obj
+    ];
+    respond(400, $response, 'fail');
 }
 function respond_user_error_msg($msg){
     $data_obj = [
@@ -123,5 +121,8 @@ function respond_user_error_msg($msg){
     respond(400, $data_obj, 'fail');
 }
 function respond_success($data_obj){
-    respond(200, $data_obj, 'success');
+    $response = [
+        'data'=>$data_obj
+    ];
+    respond(200, $response, 'success');
 }
